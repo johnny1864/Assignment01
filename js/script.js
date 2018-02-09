9859f635ff18f045d9ba6792e11c013e87206666
johnny1864 <johnny1864@yahoo.com>
2018-02-08 20:14:35 -0800

    Completed
    Finished assignment 1

---
+++ js/script.js
@@ -0,0 +1,165 @@
+/*eslint-env browser*/
+//Step 1
+
+/*
+    var someMonth;
+    function theMonth 		// function to return current month
+    currentMonth 		// a constant
+    var summerMonth; 		// an array of summer months
+    myLibrary function 	// a function 
+*/
+
+//Step 2
+    //99
+    //'string'
+    //true
+    //null
+
+
+//Step 3
+//    var expression = 15 + 5 - (5/5);
+//    var varExpression = expression + 15;
+
+//Step 4 
+    //var    firstName; 
+    //var    lastName; 
+    //var    address; 
+    //var    city; 
+    //var    state; 
+    //var    zipCode; 
+    //var    yourAge; 
+    //var    referralSource; 
+    //var    mayWeContactYou;
+
+//Step 5
+
+//    var    firstName; 
+//    var    lastName; 
+//    var    address; 
+//        
+//        firstName = 'Johnny';
+//        lastName = 'Rivera';
+//        address = '4444 44th street';
+//
+//    var    city = 'San Diego'; 
+//    var    state = 'CA'; 
+//    var    zipCode = '92111'; 
+//
+//    var    yourAge = 26, referralSource = 'friend', mayWeContactYou = 'No';
+
+//Step 6
+//    var firstVar = '9' + 9;
+//    console.log(firstVar);
+
+//    var secondVar = false + ' five';
+//    console.log(secondVar);
+
+//      var thirdVar = 5 + true;
+//      console.log(thirdVar);
+
+//Step 7
+//        console.log(numberVar);
+//        numberVar = 20;
+
+//Step 8
+
+//No it is not valid
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//window.console.log(someString);
+
+//Step 9
+
+//    var nullVar = null;
+//    console.log(nullVar);
+//
+//    var undefinedVar = undefined;
+//    console.log(undefinedVar);
+
+//Step 10
+    
+//    console.log(typeof 9);
+//    console.log(typeof 'name');
+//    console.log(typeof true);
+//    console.log(typeof {});
+//    console.log(typeof undefined);
+
+//Step 11
+//alert('Hello' + ' Johnny Rivera' + ', welcome to the JavaScript class!');
+
+//Step 12
+//var name = 'Johnny';
+//alert('Hello ' + name + ', welcome to the JavaScript class!');
+
+//Step 13
+//var name = 'Johnny';
+//var course = 'JavaScript';
+//alert('Hello ' + name + ', welcome to the ' + course + ' class!');
+
+//Step 14
+//var name = 'Johnny';
+//var course = 'JavaScript';
+//alert('Hello ' + name + ', \nWelcome to the ' + course + ' class!');
+
+//Step 15
+//var name = prompt('What is you name?');
+
+//Step 16
+//var course = prompt('What class are you taking?');
+
+//Step 17
+//    var x,
+//        y;
+//    x = 10;
+//    y = 20;
+//    console.log(x+y);
+
+//Step 18
+//    var x = 20;
+//    x += 20;
+//    console.log(x);
+
+//Step 19
+//    var x = 20;
+//    x *= 5;
+//    console.log(x);
+
+//step 20
+//    var x = 20%3;
+//    x /= 1;
+//    console.log(x);
+
+//Step 21
+//  var x=10;
+//  if(x==10 && x%2==0){
+//      console.log(x + " is equal to ten and even");
+//  }
+
+//Step 22
+
+//    var y = 55;
+//    if (y>55 || y!==55){
+//        console.log(y + ' is greater than 55');
+//    }
+
+//Step 23 
+//    var widget = new Object();
+//    console.log(typeof widget);
+
+//Step 24
+//    var widget = new Object();
+//    if (typeof widget == 'object'){
+//        console.log('widget is and object');
+//    }
+
+//Step 25
+    
+    //    var widget = new Object();
+    //    if (typeof widget == 'string'){
+    //        console.log('widget is and object');
+    //    }
+
+
+
+
+
+
